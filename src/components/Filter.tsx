38ebca9ff2be096d7b4162ab7b67085f7cae7180
Segundavid-Dev <segdavid03@gmail.com>
2025-05-20 16:07:06 +0100

    deleted filterSelect component

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,11 +1,9 @@
 import FilterSearch from "./FilterSearch";
-import FilterSelect from "./FilterSelect";
 
 export default function Filter() {
   return (
     <div className="mt-5 p-5 rounded-lg border border-gray-400">
       <FilterSearch />
-      <FilterSelect />
     </div>
   );
 }
