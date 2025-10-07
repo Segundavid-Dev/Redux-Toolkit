dc4511dd64de32d1dc9ded9dd8435709c9da7e74
Segundavid-Dev <segdavid03@gmail.com>
2025-10-07 22:05:22 +0100

    feat: start express server

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -1,1 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+
+// configure env for express app
+dotenv.config();
+// load port from env
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(PORT, () => {
+  console.log(`server started at port ${PORT} `);
+});
