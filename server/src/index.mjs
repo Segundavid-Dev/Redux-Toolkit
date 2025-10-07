b1f7b0fd4ed5bcfdddc6035a83f04170577523e6
Segundavid-Dev <segdavid03@gmail.com>
2025-10-07 22:59:58 +0100

    feat: read file and parse into JSON object

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -1,17 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
+import { readFileSync } from "node:fs";
+
+const app = express();
+
+// middleware to be able to parse request body
+app.use(express.json());
 
 // configure env for express app
 dotenv.config();
 // load port from env
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+// reading files async is the more preferred way in express JS to keep the server responsive but i will be using the sync method so as to reuse the data across each request
+const todos = JSON.parse(readFileSync("../server/data/todos.json", "utf-8"));
+// convert into a json object
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send(todos);
 });
 
+// perform CRUD OPERATIONS USING HTTP METHODS (create, read, update, delete)
+// app.get("/api/todos", ());
+
+// run the server on specified port
 app.listen(PORT, () => {
   console.log(`server started at port ${PORT} `);
 });
